a7a9af31a849312765a3644e3a6a35aef7362333
LeydiSusana <a3521110283@alumno.uttehuacan.edu.mx>
2023-04-17 19:04:58 -0600

    actualizar

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
 									<td><?php echo $item->nombre; ?></td>
 									<td><?php echo $item->fecha_nacimiento; ?></td>
 									<td class="text-center">
-										<form action="./actualizar.php" method="POST">
+										<form action="" method="post">
 											<input type="text" hidden value="<?php echo $item->_id ?>" name="id">
 											<button class="btn btn-warning">
 												<i class="fa-solid fa-user-pen"></i>
